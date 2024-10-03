05b04af78ba4089c2628d73c3a01f532ce9f160d
Rodgers31 <otienor986@gmail.com>
2024-10-03 18:31:01 -0500

    Day 8 complete

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -1,5 +1,5 @@
 def my_function():
-    for i in range(1, 20):
+    for i in range(1, 21):
         if i == 20:
             print("You got it")
 
@@ -7,6 +7,6 @@ def my_function():
 my_function()
 
 # Describe the Problem - Write your answers as comments:
-# 1. What is the for loop doing?
-# 2. When is the function meant to print "You got it"?
-# 3. What are your assumptions about the value of i?
+# 1. What is the for loop doing? loop is picking a numbers that are between 1 and 20 but not equal to 20
+# 2. When is the function meant to print "You got it"? then print :You got it" only when I is equal to 20
+# 3. What are your assumptions about the value of i? That it's between 1 and 20 but not equal to 20
